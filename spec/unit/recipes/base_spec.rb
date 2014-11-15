ac38c9520e410bb98aaaf8d2d17733b32b2fb385
Arthur Maltson <arthur@maltson.com>
2014-11-15 14:33:44 -0500

    Run tests against all defined packages.

--- spec/unit/recipes/base_spec.rb
+++ spec/unit/recipes/base_spec.rb
@@ -5,11 +5,15 @@ describe 'workstation::base' do
 
   it 'installs homebrew packages' do
     stub_command('which git').and_return('/usr/local/bin/git')
-    expect(chef_run).to install_package('autojump')
+    chef_run.node.homebrew.formulas.each do |package|
+      expect(chef_run).to install_package(package)
+    end
   end
 
   it 'installs homebrew cask packages' do
     stub_command('which git').and_return('/usr/local/bin/git')
-    expect(chef_run).to install_cask('google-chrome')
+    chef_run.node.homebrew.casks.each do |package|
+      expect(chef_run).to install_cask(package)
+    end
   end
 end
