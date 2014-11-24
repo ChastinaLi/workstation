95a0d7d6420fe8abe485fb8629d89cb8fd47b574
Arthur Maltson <arthur@maltson.com>
2014-11-23 21:53:56 -0500

    Install fan control
    Issue #6

--- recipes/imac.rb
+++ recipes/imac.rb
@@ -9,3 +9,8 @@ chef_gem 'lunchy'
 cookbook_file 'fancontrol.plist' do
   path "#{Chef::Config[:file_cache_path]}/fancontrol.plist"
 end
+
+bash 'install fancontrol' do
+  command "$HOME/.chefdk/gem/ruby/2.1.0/bin/lunchy install " \
+    "#{Chef::Config[:file_cache_path]}/fancontrol.plist"
+end
--- spec/unit/recipes/imac_spec.rb
+++ spec/unit/recipes/imac_spec.rb
@@ -12,4 +12,10 @@ describe 'workstation::imac' do
       "#{Chef::Config[:file_cache_path]}/fancontrol.plist").with_content(
         /smc -k F1Mx -w 1000/)
   end
+
+  it 'should install fancontrol' do
+    expect(chef_run).to run_bash(
+      "$HOME/.chefdk/gem/ruby/2.1.0/bin/lunchy install " \
+      "#{Chef::Config[:file_cache_path]}/fancontrol.plist")
+  end
 end
