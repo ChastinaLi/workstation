7464f03e8853fe34bdcff62267b0f2800f343db3
Arthur Maltson <arthur@maltson.com>
2016-11-27 16:03:50 -0500

    Update Homebrew recipe we use
    This one is updated for more modern homebrew built in support in Chef.

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures an OS X workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
-depends 'homebrew', '1.9.2'
+depends 'homebrew', '~> 2.1'
