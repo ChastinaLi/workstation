e11bf0a25ae5ab92432b17a853720d78c0994166
Arthur Maltson <arthur@maltson.com>
2016-11-29 06:44:18 -0500

    Put all attributes into one file
    This is just easier to manage.

--- attributes/default.rb
+++ attributes/default.rb
@@ -43,3 +43,20 @@ default['workstation']['formulas'] = %w(
     youtube-dl
     zsh
 )
+
+default['homebrew']['casks'] = %w(
+    anybar
+    atom
+    caffeine
+    fantastical
+    firefox
+    fitbit-connect
+    flux
+    github
+    handbrake
+    iterm2
+    licecap
+    macvim
+    seil
+    tomighty
+)
