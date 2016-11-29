d1b997a7db388590901a242b018e6d3bc2a1c3d5
Arthur Maltson <arthur@maltson.com>
2016-11-29 07:05:53 -0500

    Install homebrew if it's not there already

--- recipes/base.rb
+++ recipes/base.rb
@@ -1,3 +1,10 @@
+execute 'install_homebrew' do
+  not_if 'type brew > /dev/null'
+
+  command '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+  user node['workstation']['user']
+end
+
 node['workstation']['formulas'].each do |formula|
   package formula
 end
