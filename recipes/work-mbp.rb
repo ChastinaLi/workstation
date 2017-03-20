d5fe6947752a7075750712a87c30a29c85bb67ca
Chastina Li <li.chastina@gmail.com>
2017-03-20 12:23:07 -0400

    Add git specific commands

--- recipes/work-mbp.rb
+++ recipes/work-mbp.rb
@@ -3,3 +3,11 @@ include_recipe 'workstation::base'
 %w(controlplane).each do |app|
   homebrew_cask app
 end
+
+execute 'git_configs' do
+  command 'git config --global core.editor "/usr/bin/vim"'
+end
+
+remote_file '~/.vimrc' do
+  source 'https://raw.githubusercontent.com/amix/vimrc/master/vimrcs/basic.vim'
+end
